f651ac798da5bede025f5d7c7182819496cdbc5a
Sascha Falk <sascha@falk-online.eu>
2020-03-31 14:28:01 +0200

    Remove build.vs integration

--- src/CommonAssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Griffin+")]
-[assembly: AssemblyProduct("Griffin+ Common Library")]
-[assembly: AssemblyCopyright("Copyright (c) Sascha Falk 2019")]
