859d280300bfb3129198dfa0ecb20618425c8335
Sascha Falk <sascha@falk-online.eu>
2020-08-05 14:14:50 +0200

    Revert "Fix sending large messages"
    This reverts commit 04ca1187f5050cd711e568d998867cb37b98ae45.

--- src/GriffinPlus.Lib.Logging.LocalLogServicePipelineStage/LocalLogServiceConnection.cs
+++ src/GriffinPlus.Lib.Logging.LocalLogServicePipelineStage/LocalLogServiceConnection.cs
@@ -977,7 +977,7 @@ namespace GriffinPlus.Lib.Logging
 							block->Message.Message[charsToCopy] = (char) 0;
 						}
 
-						if (charsToCopy <= LogEntryBlock_Message.MessageSize / sizeof(char))
+						if (charsToCopy <= LogEntryBlock_Message.MessageSize)
 						{
 							// message fits into a single buffer
 							// => enqueue block
@@ -1052,7 +1052,7 @@ namespace GriffinPlus.Lib.Logging
 								blocks[i]->Type = LogEntryBlockType.MessageExtension;
 								blocks[i]->Reserved = 0;
 
-								charsToCopy = Math.Min(charsRemaining, LogEntryBlock_MessageExtension.MessageSize / sizeof(char));
+								charsToCopy = Math.Min(LogEntryBlock_MessageExtension.MessageSize, charsRemaining);
 
 								Buffer.MemoryCopy(
 									pMessage + offset,
@@ -1060,7 +1060,7 @@ namespace GriffinPlus.Lib.Logging
 									LogEntryBlock_MessageExtension.MessageSize * sizeof(char),
 									charsToCopy * sizeof(char));
 
-								if (charsToCopy < LogEntryBlock_MessageExtension.MessageSize / sizeof(char))
+								if (charsToCopy < LogEntryBlock_MessageExtension.MessageSize)
 								{
 									block->MessageExtension.Message[charsToCopy] = (char) 0;
 								}
