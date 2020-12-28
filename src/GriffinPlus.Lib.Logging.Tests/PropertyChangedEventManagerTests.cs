8689f9b3fa6657d3ceb5282539ff3a296ea12d53
Sascha Falk <sascha@falk-online.eu>
2020-12-28 13:22:20 +0100

    Add PropertyChangedEventManager class
    This class assists with invoking PropertyChanged event handlers in the context of the thread registering the handler.

---
+++ src/GriffinPlus.Lib.Logging.Tests/PropertyChangedEventManagerTests.cs
@@ -0,0 +1,338 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/griffinplus/dotnet-libs-logging)
+// The source code is licensed under the MIT license.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using GriffinPlus.Lib.Threading;
+
+using Xunit;
+
+namespace GriffinPlus.Lib.Logging
+{
+
+	/// <summary>
+	/// Unit tests targeting the <see cref="PropertyChangedEventManager" /> class.
+	/// </summary>
+	public class PropertyChangedEventManagerTests : IDisposable
+	{
+		private const string PropertyName = "MyProperty";
+
+		private AsyncContextThread mThread;
+
+		public class TestEventRecipient
+		{
+			public string MyString { get; set; }
+
+			public void EH_MyEvent(object sender, EventManagerEventArgs e)
+			{
+				MyString = e.MyString;
+			}
+		}
+
+		/// <summary>
+		/// Initializes an instance the <see cref="PropertyChangedEventManagerTests" /> class performing common initialization before running a test.
+		/// </summary>
+		public PropertyChangedEventManagerTests()
+		{
+			mThread = new AsyncContextThread();
+		}
+
+		/// <summary>
+		/// Cleans up.
+		/// </summary>
+		public void Dispose()
+		{
+			if (mThread != null)
+			{
+				mThread.Dispose();
+				mThread = null;
+			}
+		}
+
+		/// <summary>
+		/// Tests registering, firing and unregistering an event without using a synchronization context.
+		/// All operations are performed on the same thread.
+		/// </summary>
+		[Fact]
+		public void Complete_WithoutSynchronizationContext()
+		{
+			// the event handler
+			string changedPropertyName = null;
+
+			void Handler(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName = e.PropertyName;
+			}
+
+			// register event handler
+			int regCount = PropertyChangedEventManager.RegisterEventHandler(this, Handler, null);
+			Assert.Equal(1, regCount);
+
+			// check whether the handler is registered
+			Assert.True(PropertyChangedEventManager.IsHandlerRegistered(this));
+
+			// fire event (handler is called synchronously)
+			PropertyChangedEventManager.FireEvent(this, PropertyName);
+			Assert.Equal(PropertyName, changedPropertyName);
+
+			// unregister event handler
+			regCount = PropertyChangedEventManager.UnregisterEventHandler(this, Handler);
+			Assert.Equal(0, regCount);
+
+			// check whether the handler is not registered any more
+			Assert.False(PropertyChangedEventManager.IsHandlerRegistered(this));
+		}
+
+
+		/// <summary>
+		/// Tests registering with firing immediately and unregistering an event without using a synchronization context.
+		/// All operations are performed on the same thread.
+		/// </summary>
+		[Fact]
+		public void Complete_WithoutSynchronizationContext_FireImmediately()
+		{
+			// the event handler
+			string changedPropertyName = null;
+
+			void Handler(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName = e.PropertyName;
+			}
+
+			// register event handler and fire it immediately
+			int regCount = PropertyChangedEventManager.RegisterEventHandler(this, Handler, null, true, this, PropertyName);
+			Assert.Equal(1, regCount);
+			Assert.Equal(PropertyName, changedPropertyName);
+
+			// check whether the handler is registered
+			Assert.True(PropertyChangedEventManager.IsHandlerRegistered(this));
+
+			// unregister event handler
+			regCount = PropertyChangedEventManager.UnregisterEventHandler(this, Handler);
+			Assert.Equal(0, regCount);
+
+			// check whether the handler is not registered any more
+			Assert.False(PropertyChangedEventManager.IsHandlerRegistered(this));
+		}
+
+		/// <summary>
+		/// Tests registering, firing and unregistering an event using a synchronization context.
+		/// The event handler is called in the context of another thread.
+		/// </summary>
+		[Fact]
+		public async Task Complete_WithSynchronizationContext()
+		{
+			string changedPropertyName = null;
+			var gotEventData = new ManualResetEventSlim();
+
+			// the event handler that is expected to be called
+			void Handler(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName = e.PropertyName;
+				gotEventData.Set();
+			}
+
+			// register event handler
+			await mThread.Factory.Run(
+				() =>
+				{
+					Assert.NotNull(SynchronizationContext.Current);
+					int regCount1 = PropertyChangedEventManager.RegisterEventHandler(this, Handler, SynchronizationContext.Current);
+					Assert.Equal(1, regCount1);
+				});
+
+			// check whether the handler is registered
+			Assert.True(PropertyChangedEventManager.IsHandlerRegistered(this));
+
+			// fire event (handler is called asynchronously)
+			PropertyChangedEventManager.FireEvent(this, PropertyName);
+			Assert.True(gotEventData.Wait(200), "The event was not called asynchronously.");
+			Assert.Equal(PropertyName, changedPropertyName);
+
+			// unregister event handler
+			int regCount2 = PropertyChangedEventManager.UnregisterEventHandler(this, Handler);
+			Assert.Equal(0, regCount2);
+
+			// check whether the handler is not registered any more
+			Assert.False(PropertyChangedEventManager.IsHandlerRegistered(this));
+		}
+
+		/// <summary>
+		/// Tests registering with firing immediately and unregistering an event using a synchronization context.
+		/// The event handler is called in the context of another thread.
+		/// </summary>
+		[Fact]
+		public async Task Complete_WithSynchronizationContext_FireImmediately()
+		{
+			string changedPropertyName = null;
+			var gotEventData = new ManualResetEventSlim();
+
+			// the event handler that is expected to be called
+			void Handler(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName = e.PropertyName;
+				gotEventData.Set();
+			}
+
+			// register event handler and let it fire immediately
+			await mThread.Factory.Run(
+				() =>
+				{
+					Assert.NotNull(SynchronizationContext.Current);
+					int regCount1 = PropertyChangedEventManager.RegisterEventHandler(this, Handler, SynchronizationContext.Current, true, this, PropertyName);
+					Assert.Equal(1, regCount1);
+				});
+
+			// check whether the event was fired asynchronously
+			Assert.True(gotEventData.Wait(200), "The event was not called asynchronously.");
+			Assert.Equal(PropertyName, changedPropertyName);
+
+			// check whether the handler is registered
+			Assert.True(PropertyChangedEventManager.IsHandlerRegistered(this));
+
+			// unregister event handler
+			int regCount2 = PropertyChangedEventManager.UnregisterEventHandler(this, Handler);
+			Assert.Equal(0, regCount2);
+
+			// check whether the handler is not registered any more
+			Assert.False(PropertyChangedEventManager.IsHandlerRegistered(this));
+		}
+
+
+		/// <summary>
+		/// Tests getting a multicast delegate executing the registered event handlers for a specific event.
+		/// </summary>
+		[Fact]
+		public void GetEventCallers_WithoutSynchronizationContext()
+		{
+			string changedPropertyName1 = null;
+			string changedPropertyName2 = null;
+
+			// event handler 1 that is expected to be called
+			void Handler1(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName1 = e.PropertyName;
+			}
+
+			// event handler 2 that is expected to be called
+			void Handler2(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName2 = e.PropertyName;
+			}
+
+			// register event handlers
+			PropertyChangedEventManager.RegisterEventHandler(this, Handler1, null);
+			PropertyChangedEventManager.RegisterEventHandler(this, Handler2, null);
+
+			var callers = PropertyChangedEventManager.GetEventCallers(this);
+			Assert.NotNull(callers);
+			var delegates = callers.GetInvocationList().Cast<PropertyChangedEventHandler>().ToArray();
+			Assert.Equal(2, delegates.Length);
+
+			// call handlers
+			delegates[0](this, new PropertyChangedEventArgs("Test1"));
+			delegates[1](this, new PropertyChangedEventArgs("Test2"));
+			Assert.Equal("Test1", changedPropertyName1);
+			Assert.Equal("Test2", changedPropertyName2);
+		}
+
+
+		/// <summary>
+		/// Tests getting a multicast delegate executing the registered event handlers for a specific event.
+		/// </summary>
+		[Fact]
+		public async Task GetEventCallers_WithSynchronizationContext()
+		{
+			string changedPropertyName1 = null;
+			string changedPropertyName2 = null;
+			var gotEventData1 = new ManualResetEventSlim();
+			var gotEventData2 = new ManualResetEventSlim();
+
+			// event handler 1 that is expected to be called
+			void Handler1(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName1 = e.PropertyName;
+				gotEventData1.Set();
+			}
+
+			// event handler 1 that is expected to be called
+			void Handler2(object sender, PropertyChangedEventArgs e)
+			{
+				changedPropertyName2 = e.PropertyName;
+				gotEventData2.Set();
+			}
+
+			// register event handlers:
+			// - register handler1 only, but do not trigger firing immediately
+			// - register handler2 and trigger firing immediately
+			await mThread.Factory.Run(
+				() =>
+				{
+					Assert.NotNull(SynchronizationContext.Current);
+					PropertyChangedEventManager.RegisterEventHandler(this, Handler1, SynchronizationContext.Current);
+					Assert.False(gotEventData1.IsSet, "Event handler was called unexpectedly.");
+					PropertyChangedEventManager.RegisterEventHandler(this, Handler2, SynchronizationContext.Current, true, this, "Test2");
+					Assert.False(gotEventData1.IsSet, "Event handler was called immediately, should have been scheduled to be executed...");
+				});
+
+			// only handler2 should have been called after some time
+			Assert.False(gotEventData1.Wait(200), "The event was called unexpectedly.");
+			Assert.True(gotEventData2.Wait(200), "The event was not called asynchronously.");
+			Assert.Null(changedPropertyName1);
+			Assert.Equal("Test2", changedPropertyName2);
+
+			// get delegates invoking the event handlers
+			var callers = PropertyChangedEventManager.GetEventCallers(this);
+			Assert.NotNull(callers);
+			var delegates = callers.GetInvocationList().Cast<PropertyChangedEventHandler>().ToArray();
+			Assert.Equal(2, delegates.Length);
+
+			// call handlers
+			gotEventData1.Reset();
+			gotEventData2.Reset();
+			delegates[0](this, new PropertyChangedEventArgs("Test1"));
+			delegates[1](this, new PropertyChangedEventArgs("Test2"));
+			Assert.True(gotEventData1.Wait(200), "The event was not called asynchronously.");
+			Assert.True(gotEventData2.Wait(200), "The event was not called asynchronously.");
+			Assert.Equal("Test1", changedPropertyName1);
+			Assert.Equal("Test2", changedPropertyName2);
+		}
+
+
+		/// <summary>
+		/// Checks whether the event manager detects and cleans up objects that have registered events, but have been garbage collected.
+		/// </summary>
+		[Fact]
+		public void EnsureEventProvidersAreCollectable()
+		{
+			// the event handler
+			void Handler(object sender, PropertyChangedEventArgs e)
+			{
+			}
+
+			// register an event handler to a dummy event provider object
+			// (must not be done in the same method to allow the object to be collected in the next step)
+			var weakReferenceProvider = new Func<WeakReference>(
+				() =>
+				{
+					var provider = new object();
+					int regCount = PropertyChangedEventManager.RegisterEventHandler(provider, Handler, null);
+					Assert.Equal(1, regCount);
+					return new WeakReference(provider);
+				}).Invoke();
+
+			// kick object out of memory
+			GC.Collect();
+
+			// the event provider should now be collected
+			Assert.False(weakReferenceProvider.IsAlive);
+		}
+	}
+
+}
---
+++ src/GriffinPlus.Lib.Logging/PropertyChangedEventManager.cs
@@ -0,0 +1,273 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file is part of the Griffin+ common library suite (https://github.com/griffinplus/dotnet-libs-logging)
+// The source code is licensed under the MIT license.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace GriffinPlus.Lib.Logging
+{
+
+	/// <summary>
+	/// Event manager that administrates event handlers in a central place.
+	/// Specifically made for <see cref="INotifyPropertyChanged.PropertyChanged" /> events.
+	/// Objects firing events do not need to implement own event add/remove logic, especially when it comes to firing
+	/// events asynchronously.
+	/// </summary>
+	static class PropertyChangedEventManager
+	{
+		#region Internal Data Types
+
+		/// <summary>
+		/// An event handler item in the event manager.
+		/// </summary>
+		private readonly struct Item
+		{
+			public readonly SynchronizationContext      SynchronizationContext;
+			public readonly PropertyChangedEventHandler Handler;
+
+			public Item(SynchronizationContext context, PropertyChangedEventHandler handler)
+			{
+				SynchronizationContext = context;
+				Handler = handler;
+			}
+		}
+
+		#endregion
+
+		#region Class Variables
+
+		private static readonly ConditionalWeakTable<object, Item[]> sItemsByObject = new ConditionalWeakTable<object, Item[]>();
+		private static readonly object                               sSync          = new object();
+
+		#endregion
+
+		/// <summary>
+		/// Registers an event handler for an event associated with the specified object.
+		/// </summary>
+		/// <param name="obj">Object providing the event.</param>
+		/// <param name="handler">Event handler to register.</param>
+		/// <param name="context">
+		/// Synchronization context to use when calling the event handler
+		/// (<c>null</c> to execute the event handler in the context of the thread firing the event).
+		/// </param>
+		/// <returns>Total number of registered event handlers (including the specified event handler).</returns>
+		public static int RegisterEventHandler(
+			object                      obj,
+			PropertyChangedEventHandler handler,
+			SynchronizationContext      context)
+		{
+			return RegisterEventHandler(obj, handler, context, false, null, null);
+		}
+
+		/// <summary>
+		/// Registers an event handler for an event associated with the specified object and optionally fires the
+		/// event handler after registration.
+		/// </summary>
+		/// <param name="obj">Object providing the event.</param>
+		/// <param name="handler">Event handler to register.</param>
+		/// <param name="context">
+		/// Synchronization context to use when calling the event handler
+		/// (<c>null</c> to execute the event handler in the context of the thread firing the event).
+		/// </param>
+		/// <param name="fireImmediately">
+		/// true to register and fire the event handler immediately after registration;
+		/// false to register the event handler only.
+		/// </param>
+		/// <param name="sender">
+		/// Sender object to pass to the event handler that is fired immediately.
+		/// </param>
+		/// <param name="propertyName">
+		/// Name of the property that has changed (is passed to the event handler that is fired immediately).
+		/// </param>
+		/// <returns>
+		/// Total number of registered event handlers (including the specified event handler).
+		/// </returns>
+		public static int RegisterEventHandler(
+			object                      obj,
+			PropertyChangedEventHandler handler,
+			SynchronizationContext      context,
+			bool                        fireImmediately,
+			object                      sender,
+			string                      propertyName)
+		{
+			Item[] newItems;
+
+			lock (sSync)
+			{
+				if (sItemsByObject.TryGetValue(obj, out var items))
+				{
+					newItems = new Item[items.Length + 1];
+					Array.Copy(items, newItems, items.Length);
+					newItems[items.Length] = new Item(context, handler);
+					sItemsByObject.Remove(obj);
+					sItemsByObject.Add(obj, newItems);
+				}
+				else
+				{
+					newItems = new[] { new Item(context, handler) };
+					sItemsByObject.Remove(obj);
+					sItemsByObject.Add(obj, newItems);
+				}
+			}
+
+			if (fireImmediately)
+			{
+				if (context != null)
+				{
+					context.Post(x => { handler(sender, new PropertyChangedEventArgs(propertyName)); }, null);
+				}
+				else
+				{
+					handler(sender, new PropertyChangedEventArgs(propertyName));
+				}
+			}
+
+			return newItems.Length;
+		}
+
+		/// <summary>
+		/// Unregisters an event handler from the specified event.
+		/// </summary>
+		/// <param name="obj">Object providing the event.</param>
+		/// <param name="handler">Event handler to unregister.</param>
+		/// <returns>
+		/// Total number of registered event handlers after the specified handler has been removed;
+		/// -1, if the specified event handler is not registered.
+		/// </returns>
+		public static int UnregisterEventHandler(object obj, PropertyChangedEventHandler handler)
+		{
+			lock (sSync)
+			{
+				if (!sItemsByObject.TryGetValue(obj, out var items))
+				{
+					// specified event handler was not registered
+					return -1;
+				}
+
+				for (int i = 0; i < items.Length; i++)
+				{
+					var registeredHandler = items[i].Handler;
+					if (registeredHandler == handler)
+					{
+						var newItems = new Item[items.Length - 1];
+						for (int j = 0, k = 0; j < items.Length; j++)
+						{
+							if (j != i) newItems[k++] = items[j];
+						}
+
+						if (newItems.Length > 0)
+						{
+							sItemsByObject.Remove(obj);
+							sItemsByObject.Add(obj, newItems);
+							return newItems.Length;
+						}
+
+						sItemsByObject.Remove(obj);
+						return 0;
+					}
+				}
+
+				// specified event handler was not registered
+				return -1;
+			}
+		}
+
+		/// <summary>
+		/// Checks whether the specified event has event handlers attached to it.
+		/// </summary>
+		/// <param name="obj">Object providing the event.</param>
+		/// <returns>true, if the specified event has event handlers attached; otherwise false.</returns>
+		public static bool IsHandlerRegistered(object obj)
+		{
+			lock (sSync)
+			{
+				if (!sItemsByObject.TryGetValue(obj, out _))
+				{
+					return false;
+				}
+
+				return true;
+			}
+		}
+
+		/// <summary>
+		/// Fires the <see cref="INotifyPropertyChanged.PropertyChanged" /> event invoking all event handlers that
+		/// are attached to it (event handlers that are associated with a synchronization context are executed in
+		/// the thread the synchronization context belongs to).
+		/// </summary>
+		/// <param name="obj">
+		/// Object providing the event (is passed as the 'sender' object to the event handler as well).
+		/// </param>
+		/// <param name="propertyName">Name of the property that has changed.</param>
+		public static void FireEvent(object obj, string propertyName)
+		{
+			Item[] items;
+
+			lock (sSync)
+			{
+				if (!sItemsByObject.TryGetValue(obj, out items)) return;
+			}
+
+			var e = new PropertyChangedEventArgs(propertyName);
+			foreach (var item in items)
+			{
+				if (item.SynchronizationContext != null)
+				{
+					item.SynchronizationContext.Post(x => { ((Item)x).Handler(obj, e); }, item);
+				}
+				else
+				{
+					item.Handler(obj, e);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets a multicast delegate that calls all event handlers that are attached to the
+		/// <see cref="INotifyPropertyChanged.PropertyChanged" /> event (event handlers that are associated with a
+		/// synchronization context are executed in the thread the synchronization context belongs to).
+		/// </summary>
+		/// <param name="obj">Object providing the event.</param>
+		/// <returns>Event callers.</returns>
+		public static PropertyChangedEventHandler GetEventCallers(object obj)
+		{
+			Item[] items;
+
+			lock (sSync)
+			{
+				if (!sItemsByObject.TryGetValue(obj, out items))
+				{
+					return null;
+				}
+			}
+
+			PropertyChangedEventHandler handlers = null;
+
+			foreach (var item in items)
+			{
+				if (item.SynchronizationContext != null)
+				{
+					handlers += (sender, e) =>
+					{
+						item.SynchronizationContext.Post(x => { ((Item)x).Handler(sender, e); }, item);
+					};
+				}
+				else
+				{
+					var itemCopy = item;
+					handlers += (sender, e) =>
+					{
+						itemCopy.Handler(sender, e);
+					};
+				}
+			}
+
+			return handlers;
+		}
+	}
+
+}
