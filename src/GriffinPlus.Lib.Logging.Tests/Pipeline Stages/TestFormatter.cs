1a6c42ca04034a3003a0afb4ea279db275eb6e88
Sascha Falk <sascha@falk-online.eu>
2021-05-02 18:06:36 +0200

    Fix issue in test concerning timestamp formats

--- src/GriffinPlus.Lib.Logging.Tests/Pipeline Stages/TestFormatter.cs
+++ src/GriffinPlus.Lib.Logging.Tests/Pipeline Stages/TestFormatter.cs
@@ -23,7 +23,7 @@ namespace GriffinPlus.Lib.Logging
 			var builder = new StringBuilder();
 
 			builder.AppendFormat(
-				"{0} ### {1} ### {2} ### {3} ### {4} ### {5} ### {6} ### {7} ### {8}",
+				"{0:u} ### {1} ### {2} ### {3} ### {4} ### {5} ### {6} ### {7} ### {8}",
 				message.Timestamp,
 				message.HighPrecisionTimestamp,
 				message.LogWriterName,
