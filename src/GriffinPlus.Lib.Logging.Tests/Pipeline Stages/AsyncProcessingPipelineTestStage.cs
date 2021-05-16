47973d9e4828d7ee5b7762f4256528f2b329155b
Sascha Falk <sascha@falk-online.eu>
2021-05-16 08:03:49 +0200

    Fix concurrency issue in AsyncProcessingPipelineTestStage

--- src/GriffinPlus.Lib.Logging.Tests/Pipeline Stages/AsyncProcessingPipelineTestStage.cs
+++ src/GriffinPlus.Lib.Logging.Tests/Pipeline Stages/AsyncProcessingPipelineTestStage.cs
@@ -16,6 +16,14 @@ namespace GriffinPlus.Lib.Logging
 	/// </summary>
 	public class AsyncProcessingPipelineTestStage : AsyncProcessingPipelineStage<AsyncProcessingPipelineTestStage>
 	{
+		private          bool                  mOnInitializeWasCalled;
+		private          bool                  mOnShutdownWasCalled;
+		private          bool                  mProcessSyncWasCalled;
+		private          bool                  mProcessAsyncWasCalled;
+		private          LocalLogMessage       mMessagePassedToProcessSync;
+		private readonly List<LocalLogMessage> mMessagesPassedToProcessAsync = new List<LocalLogMessage>();
+		private readonly object                mSync                         = new object();
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="AsyncProcessingPipelineTestStage"/> class.
 		/// </summary>
@@ -27,66 +35,140 @@ namespace GriffinPlus.Lib.Logging
 		/// <summary>
 		/// Gets a value indicating whether <see cref="OnInitialize"/> was called.
 		/// </summary>
-		public bool OnInitializeWasCalled { get; private set; }
+		public bool OnInitializeWasCalled
+		{
+			get
+			{
+				lock (mSync)
+				{
+					return mOnInitializeWasCalled;
+				}
+			}
+			private set
+			{
+				lock (mSync)
+				{
+					mOnInitializeWasCalled = value;
+				}
+			}
+		}
 
 		/// <summary>
 		/// Gets a value indicating whether <see cref="OnShutdown"/> was called.
 		/// </summary>
-		public bool OnShutdownWasCalled { get; private set; }
+		public bool OnShutdownWasCalled
+		{
+			get
+			{
+				lock (mSync) return mOnShutdownWasCalled;
+			}
+			private set
+			{
+				lock (mSync) mOnShutdownWasCalled = value;
+			}
+		}
 
 		/// <summary>
 		/// Gets a value indicating whether <see cref="ProcessSync(LocalLogMessage, out bool)"/> was called.
 		/// </summary>
-		public bool ProcessSyncWasCalled { get; private set; }
+		public bool ProcessSyncWasCalled
+		{
+			get
+			{
+				lock (mSync) return mProcessSyncWasCalled;
+			}
+			private set
+			{
+				lock (mSync) mProcessSyncWasCalled = value;
+			}
+		}
 
 		/// <summary>
 		/// Gets the message that was passed to <see cref="AsyncProcessingPipelineStage{STAGE}.ProcessSync(LocalLogMessage, out bool)"/>
 		/// with the last call.
 		/// </summary>
-		public LocalLogMessage MessagePassedToProcessSync { get; private set; }
+		public LocalLogMessage MessagePassedToProcessSync
+		{
+			get
+			{
+				lock (mSync) return mMessagePassedToProcessSync;
+			}
+			private set
+			{
+				lock (mSync) mMessagePassedToProcessSync = value;
+			}
+		}
 
 		/// <summary>
 		/// Gets the messages that were passed to <see cref="AsyncProcessingPipelineStage{STAGE}.ProcessAsync(LocalLogMessage[], CancellationToken)"/>
 		/// with the last call.
 		/// </summary>
-		public List<LocalLogMessage> MessagesPassedToProcessAsync { get; } = new List<LocalLogMessage>();
+		public List<LocalLogMessage> MessagesPassedToProcessAsync
+		{
+			get
+			{
+				lock (mSync) return mMessagesPassedToProcessAsync;
+			}
+		}
 
 		/// <summary>
 		/// Gets a value indicating whether <see cref="ProcessAsync(LocalLogMessage[], CancellationToken)"/> was called.
 		/// </summary>
-		public bool ProcessAsyncWasCalled { get; private set; }
+		public bool ProcessAsyncWasCalled
+		{
+			get
+			{
+				lock (mSync) return mProcessAsyncWasCalled;
+			}
+			private set
+			{
+				lock (mSync) mProcessAsyncWasCalled = value;
+			}
+		}
 
 		protected override void OnInitialize()
 		{
-			OnInitializeWasCalled = true;
-			base.OnInitialize();
+			lock (mSync)
+			{
+				OnInitializeWasCalled = true;
+				base.OnInitialize();
+			}
 		}
 
 		protected internal override void OnShutdown()
 		{
-			OnShutdownWasCalled = true;
-			base.OnShutdown();
+			lock (mSync)
+			{
+				OnShutdownWasCalled = true;
+				base.OnShutdown();
+			}
 		}
 
 		protected override bool ProcessSync(LocalLogMessage message, out bool queueForAsyncProcessing)
 		{
-			ProcessSyncWasCalled = true;
-			MessagePassedToProcessSync?.Release();
-			MessagePassedToProcessSync = message;
-			MessagePassedToProcessSync.AddRef();
-			return base.ProcessSync(message, out queueForAsyncProcessing);
+			lock (mSync)
+			{
+				ProcessSyncWasCalled = true;
+				MessagePassedToProcessSync?.Release();
+				MessagePassedToProcessSync = message;
+				MessagePassedToProcessSync.AddRef();
+				return base.ProcessSync(message, out queueForAsyncProcessing);
+			}
 		}
 
 		protected override Task ProcessAsync(LocalLogMessage[] messages, CancellationToken cancellationToken)
 		{
-			ProcessAsyncWasCalled = true;
+			lock (mSync)
+			{
+				ProcessAsyncWasCalled = true;
 
-			MessagesPassedToProcessAsync.ForEach(x => x.Release());
-			MessagesPassedToProcessAsync.Clear();
-			MessagesPassedToProcessAsync.AddRange(messages);
-			MessagesPassedToProcessAsync.ForEach(x => x.AddRef());
+				MessagesPassedToProcessAsync.ForEach(x => x.Release());
+				MessagesPassedToProcessAsync.Clear();
+				MessagesPassedToProcessAsync.AddRange(messages);
+				MessagesPassedToProcessAsync.ForEach(x => x.AddRef());
 
-			return base.ProcessAsync(messages, cancellationToken);
+				return base.ProcessAsync(messages, cancellationToken);
+			}
 		}
 	}
 
